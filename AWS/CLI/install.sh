2449b4e8eae5656e3786325604a2fc999d75ea14
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-01 17:47:52 -0500

    Create install.sh cli

---
+++ AWS/CLI/install.sh
@@ -0,0 +1,7 @@
+# Install aws-cli on your machine and execute the following commands to set up AWS CLI operations.
+# Install or update to the latest version of the AWS CLI
+# Check on AWS webpages
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
