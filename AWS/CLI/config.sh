55811aeef1b266865770c3f3cbb38cd3964e5678
Jose Osuna Barrios <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-01 17:50:07 -0500

    Delete AWS/CLI/config.sh

--- AWS/CLI/config.sh
+++
@@ -1,5 +0,0 @@
-# Install aws-cli on your machine and execute the following commands to set up AWS CLI operations.
-
- aws configure set aws_access_key_id your_aws_acces_key_id
- aws configure set aws_secret_access_key your_aws_secret_access_key
- aws configure set region your_aws_region
