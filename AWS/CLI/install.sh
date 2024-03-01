1f7bcfc802a852eb8dea2f436c262f51cd53fd4d
Jose Osuna Barrios <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-01 17:55:56 -0500

    Update install.sh add config

--- AWS/CLI/install.sh
+++ AWS/CLI/install.sh
@@ -10,4 +10,4 @@ sudo ./aws/install
 
  aws configure set aws_access_key_id your_aws_acces_key_id
  aws configure set aws_secret_access_key your_aws_secret_access_key
- aws configure set region your_aws_region
+ aws configure set region us-east-1
