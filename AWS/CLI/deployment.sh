7d5c9af68c64b95c245bf45ebd28907a8fd5884e
Jose Osuna Barrios <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-04 12:12:11 -0500

    Delete AWS/CLI directory

--- AWS/CLI/deployment.sh
+++
@@ -1,4 +0,0 @@
-# validate template
-aws cloudformation validate-template --template-body file://deployment_template.yml
-# deploy template
-aws cloudformation deploy --stack-name static-hosting-demo-stack --template-file deployment_template.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
